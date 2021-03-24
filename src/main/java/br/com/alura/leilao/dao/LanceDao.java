d77100b4bd1c6adf2ec4b7bea759a266f4283c0f
André Rodrigues <andre88.rg@gmail.com>
2021-03-24 09:43:27 -0300

    refatoração classes DAO

--- src/main/java/br/com/alura/leilao/dao/LanceDao.java
+++ src/main/java/br/com/alura/leilao/dao/LanceDao.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.leilao.model.Lance;
@@ -11,17 +12,21 @@ import br.com.alura.leilao.model.Leilao;
 @Repository
 public class LanceDao {
 
-	@PersistenceContext
-	private EntityManager em;
+    private EntityManager em;
 
-	public void salvar(Lance lance) {
-		em.persist(lance);
-	}
+    @Autowired
+    public LanceDao(EntityManager em) {
+        this.em = em;
+    }
 
-	public Lance buscarMaiorLanceDoLeilao(Leilao leilao) {
-		return em.createQuery("SELECT l FROM Lance l WHERE l.valor = (SELECT MAX(lance.valor) FROM Lance lance)", Lance.class)
-				.setParameter("leilao", leilao)
-				.getSingleResult();
-	}
-	
+    public void salvar(Lance lance) {
+        em.persist(lance);
+    }
+
+    public Lance buscarMaiorLanceDoLeilao(Leilao leilao) {
+        return em.createQuery("SELECT l FROM Lance l WHERE l.valor = (SELECT MAX(lance.valor) FROM Lance lance)", Lance.class)
+                .setParameter("leilao", leilao)
+                .getSingleResult();
+    }
+
 }
