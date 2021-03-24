8f08c9bac48f1e95540c58a8f856a0d5861f4420
André Rodrigues <andre88.rg@gmail.com>
2021-03-24 10:39:14 -0300

    refatoração dos testes para uma melhor legibilidade.

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -2,10 +2,12 @@ package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,20 +15,47 @@ class UsuarioDaoTest {
 
     private UsuarioDao dao;
     private EntityManager em;
+    private Usuario user;
 
     @BeforeEach
     void setUp() {
         this.em = JPAUtil.getEntityManager();
         this.dao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    void tearDown() {
+        em.getTransaction().rollback();
     }
 
     @Test
-    void buscaPorNomeDeUsuario() {
-        Usuario novoUsuario = new Usuario("fulano","fulano@email.com","123456");
-        em.getTransaction().begin();
-        em.persist(novoUsuario);
-        em.getTransaction().commit();
-        Usuario user = dao.buscarPorUsername(novoUsuario.getNome());
-        assertNotNull(user);
+    void deveriaEncontrarUsuarioCadastradoPeloNome() {
+        seRequerUsuario();
+        Usuario userEncontrado = quandoBuscarUsuario(user.getNome());
+        entaoDeveriaEncontrarUsuarioNoBanco(userEncontrado);
+    }
+
+    @Test
+    void naoDeveriaEncontrarUsuarioNaoCadastradoCadastradoPeloNome() {
+        seRequerUsuario();
+        entaoDeveriaLancarException(new NoResultException(),"ciclano");
+    }
+
+    private void seRequerUsuario() {
+        this.user = new Usuario("fulano","fulano@email.com","123456");
+        em.persist(user);
+    }
+
+    private Usuario quandoBuscarUsuario(String nome) {
+        return dao.buscarPorUsername(nome);
+    }
+
+    private void entaoDeveriaEncontrarUsuarioNoBanco(Usuario usuario) {
+        assertNotNull(usuario);
+    }
+
+    private void entaoDeveriaLancarException(Exception e, String nome) {
+        assertThrows(e.getClass(), () -> dao.buscarPorUsername(nome));
     }
 }
